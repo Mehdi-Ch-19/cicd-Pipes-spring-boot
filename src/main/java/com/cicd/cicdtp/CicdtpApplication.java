a0e25b0fceddd268cf7bac09cb21c57ddee7075c
Mehdi-Ch-19 <elmahdichiheb19@gmail.com>
2023-03-13 20:42:56 +0100

    Command line runner

--- src/main/java/com/cicd/cicdtp/CicdtpApplication.java
+++ src/main/java/com/cicd/cicdtp/CicdtpApplication.java
@@ -1,13 +1,27 @@
 package com.cicd.cicdtp;
 
+import com.cicd.cicdtp.entity.Manager;
+import com.cicd.cicdtp.service.ManagerService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CicdtpApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CicdtpApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner runner(ManagerService service){
+        return args -> {
+            service.adduser(Manager.builder()
+                            .name("mehdi")
+                            .password("haha")
+                            .email("mehdi@gmail.com")
+                    .build());
+        };
     }
 
 }
